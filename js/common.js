176298d1ec1ee50cc819e1913597206ae9f23a98
hraboviyvadim <hraboviyvadim@gmail.com>
2013-12-19 14:57:50 +0200

    menu

--- js/common.js
+++ js/common.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
 
+	// sub-menu
+	
+	$(".menu__item").hover(
+		function(){
+			$(this).find('.sub-menu').addClass('is-active');
+			$(this).find('.menu-link').addClass('is-active');
+		},
+		function(){
+			$(this).find('.sub-menu').removeClass('is-active');
+			$(this).find('.menu-link').removeClass('is-active');
+		}
+	);
+
 });
