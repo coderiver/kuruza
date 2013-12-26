760be77bff89758f227db1f1d5a65e2fc03abdfe
hraboviyvadim <hraboviyvadim@gmail.com>
2013-12-26 16:32:12 +0200

    meta position fixed

--- js/common.js
+++ js/common.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
     } 
 	
 
-    // sticky top-menu
+    // sticky elements
 
     function menu_scroll() {
     	var position = $(".js-topper").offset().top;
@@ -61,15 +61,63 @@ $(document).ready(function() {
     	menu_scroll();
     } 
     
+    var header_height = $(".topper-wrap").outerHeight();
+
+    function author_scroll() {
+    	var position2 = $(".js-article-block").offset().top;
+    	var	position_off2 = $(".article").offset().top;
+    	var pos_left = $(".article").offset().left + 770;
+
+    	if ($(window).scrollTop() >= position2 - header_height) {
+    		$(".js-article-block").addClass('fixed');
+    		$(".js-article-block").css('left', pos_left);
+    	}
+    	if ($(window).scrollTop() <= position_off2 - header_height){
+    		$(".js-article-block").removeClass('fixed');
+    		$(".js-article-block").css('left', 770);
+    	}
+    	else {}
+    }
+	
+
+    function info_scroll() {
+    	var position3 = $(".js-article-info").offset().top;
+    	var	position_off3 = $(".article").offset().top;
+    	var pos_left2 = $(".article").offset().left + 778;
+
+    	if ($(window).scrollTop() >= position3 - header_height) {
+    		$(".js-article-info").addClass('fixed');
+    		$(".js-article-info").css('right', pos_left2);
+    	}
+    	if ($(window).scrollTop() <= position_off3 - header_height){
+    		$(".js-article-info").removeClass('fixed');
+    		$(".js-article-info").css('right', 778);
+    	}
+    	else {}
+    }
+	
+	if ($(".article").length >0 ){
+    	info_scroll();
+    	author_scroll();
+    }
+
 
     $(window).scroll(function(){
       	if ($(".js-topper-wrap").length >0 ){
     		menu_scroll();
     	}
+    	if ($(".article").length >0 ){
+    		author_scroll();
+    		info_scroll();
+    	}
     });
     $(window).resize(function(){
     	if ($(".js-topper-wrap").length >0 ){
     		menu_scroll();
+    	}
+    	if ($(".article").length >0 ){
+    		author_scroll();
+    		info_scroll();
     	}
     });
 
