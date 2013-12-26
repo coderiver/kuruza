db9a2d5cd32d3e477887e3456c00143d6084766f
hraboviyvadim <hraboviyvadim@gmail.com>
2013-12-26 15:47:10 +0200

    post static done

--- js/common.js
+++ js/common.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 	// masonry bricks
 
 	$('.masonry').masonry({
-	  	columnWidth: 361,
+	  	columnWidth: 360,
 	  	gutter: 10,
 	  	itemSelector: '.post',
 	  	stamp: '.sidebar'
@@ -72,32 +72,5 @@ $(document).ready(function() {
     		menu_scroll();
     	}
     });
-
-    //sticky article info
-    // var stickyPanelSettings = {
-    //     // Use this to set the top margin of the detached panel.
-    //     topPadding: 44,
- 
-    //     // This class is applied when the panel detaches.
-    //     afterDetachCSSClass: "",
- 
-    //     // When set to true the space where the panel was is kept open.
-    //     savePanelSpace: true,
- 
-    //     // Event fires when panel is detached
-    //     // function(detachedPanel, panelSpacer){....}
-    //     onDetached: null,
- 
-    //     // Event fires when panel is reattached
-    //     // function(detachedPanel){....}
-    //     onReAttached: null,
- 
-    //     // Set this using any valid jquery selector to 
-    //     // set the parent of the sticky panel.
-    //     // If set to null then the window object will be used.
-    //     parentSelector: null
-    // };
-    
-    // $(".article__author").stickyPanel(stickyPanelSettings);
 
 });
